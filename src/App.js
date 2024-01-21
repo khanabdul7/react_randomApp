ca0defc3ca300ea5a73de49f105b80de0bfa32dd
Abdul khan <abdulmatania@gmail.com>
2024-01-21 17:09:59 +0530

    added tailwind

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { useState } from 'react';
 import './App.css';
+import '@fortawesome/fontawesome-free/css/all.css';
 import Table from './Table';
+import Banner from './Banner';
 
 function App() {
   let [value, setValue] = useState('')
-  let [names, setNames] = useState(['abdul', 'salman', 'imran'])
+  let [names, setNames] = useState(['alpha', 'arlot', 'aldous', 'thamuz', 'vale','brody'])
   const handleInputChange = (e) => {
     setValue(e.target.value)
   }
@@ -30,27 +32,35 @@ function App() {
       // console.log(`name: ${names[randomIndexWithRange]} --> index: ${randomIndexWithRange}`)
       alert(randomName)
       handleRemove(randomName)
-      if(names.length-1 === 1){
+      if (names.length - 1 === 1) {
         alert(`Winner is: ${names[0]}`)
       }
     }
-    else{
+    else {
       alert('List should have atleast 2 names !')
     }
   }
-  return (
-    <>
-      <h1>Welcome to Random App</h1>
-      <div>
-        <input type='text' id='name-input' value={value} placeholder='add names here' onChange={(e) => handleInputChange(e)}
+  return (<>
+    <div>
+      {/* needs to implement */}
+      <Banner message='message'/>
+    </div>
+    <div className='flex-col flex h-screen justify-center'>
+      <div className='bg-slate-300'>
+        <h1 className=' text-center text-3xl p-1'>Welcome to Random App</h1>
+      </div>
+      <div className="flex justify-center">
+        <input className='text-left w-80 border-2 border-black rounded-md p-2 m-2 text-base' type='text' id='name-input' value={value} placeholder='add names here' onChange={(e) => handleInputChange(e)}
           onKeyDown={(e) => { if (e.key === 'Enter') handleAdd() }}></input>
-        <button type='button' id='btn' onClick={(value) => handleAdd(value)}>Add</button>
-        <button type='button' id='random-btn' onClick={() => generateRandom()}>Random</button>
       </div>
-      <div>
-        {names.length>0 && <Table nameList={names} handleRemove={handleRemove} />}
+      <div className='flex justify-center'>
+        <button className='bg-lime-500 rounded-md w-12 h-10 mx-2' type='button' id='btn' onClick={(value) => handleAdd(value)}>Add</button>
+        <button className='bg-lime-500 rounded-md w-12 h-10 mx-2' type='button' id='random-btn' onClick={() => generateRandom()}>Get</button>
       </div>
-
+      <div className='flex justify-center mt-3'>
+        <Table nameList={names} handleRemove={handleRemove}/>
+      </div>
+    </div>
     </>
   );
 }
--- src/Table.js
+++ src/Table.js
@@ -1,24 +1,29 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faTrash} from '@fortawesome/free-solid-svg-icons'
 
 const Table = (props) => {
     let {nameList} = props
-    // console.log('namelist: ',nameList)
-    return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Names List</th>
-                    <th>Remove</th>
+    return (<>
+       {nameList.length>0 ? <table className='w-64'>
+          <thead>
+            <tr className='text-xl'>
+              <th className='border-2'>Names</th>
+              <th className='border-2'>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+             {nameList.map(name => {
+              return (
+                <tr key={name}>
+                  <td className='px-2 border-2'>{name}</td>
+                  <td className='border-2 px-2'><FontAwesomeIcon className='action' onClick={()=>props.handleRemove(name)} icon={faTrash}/></td>
                 </tr>
-            </thead>
-            <tbody>
-                {nameList && nameList.map(name =>
-                    <tr key={name}>
-                        <td>{name}</td>
-                        <td onClick={()=>props.handleRemove(name)}>X</td>
-                    </tr>
-                )}
-            </tbody>
-        </table>
+              )
+            })
+            }
+          </tbody>
+        </table> : 'Please add some names to show here'}
+        </>
     )
 }
 
